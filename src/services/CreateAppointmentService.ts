7b0749bd4fdcd47dfa3130109aeee8f6ab755d22
diegovinci <diego.vinci@outlook.com>
2020-12-26 12:50:42 -0300

    fix: update appointments route and services to receive the provider id intead of the provider name

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsReposiory = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,7 +24,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsReposiory.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
