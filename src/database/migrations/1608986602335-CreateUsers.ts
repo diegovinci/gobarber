685341ff460a94c706e7b4c827b09f8411608828
diegovinci <diego.vinci@outlook.com>
2020-12-26 11:25:04 -0300

    refactor: change the varchar ids to uuid in the tables appointments and users

--- src/database/migrations/1608986602335-CreateUsers.ts
+++ src/database/migrations/1608986602335-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1608986602335 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
