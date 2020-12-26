4a3cea6f4f628eb3ce3421b6027b50fefc88c699
diegovinci <diego.vinci@outlook.com>
2020-12-26 19:17:26 -0300

    refactor: extract authentication variables into a separate file

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '91a33ecdbc9f93633bd040fe819ddfbd',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email or password.');
     }
 
-    const token = sign({}, '91a33ecdbc9f93633bd040fe819ddfbd', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
